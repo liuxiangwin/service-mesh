fb0918223bdd7c1d3de36d0918434adbdc4cd99a
voravitl <voravitl@gmail.com>
2019-12-20 08:04:05 +0700

    V1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-oc apply -f ocp/backend-v1-deployment.yml
-oc apply -f ocp/backend-v2-deployment.yml
-oc apply -f ocp/backend-service.yml
-oc apply -f ocp/frontend-v1-deployment.yml
-#oc apply -f ocp/frontend-v2-deployment.yml
-oc apply -f ocp/frontend-service.yml
-oc apply -f ocp/frontend-route.yml
+oc apply -f ocp/backend-v1-deployment.yml -n $USERID
+oc apply -f ocp/backend-v2-deployment.yml -n $USERID
+oc apply -f ocp/backend-service.yml -n $USERID
+oc apply -f ocp/frontend-v1-deployment.yml -n $USERID
+#oc apply -f ocp/frontend-v2-deployment.yml -n $USERID
+oc apply -f ocp/frontend-service.yml -n $USERID
+oc apply -f ocp/frontend-route.yml -n $USERID
--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-oc delete -f ocp/backend-v1-deployment.yml
-oc delete -f ocp/backend-v2-deployment.yml
-oc delete -f ocp/backend-service.yml
-oc delete -f ocp/frontend-v1-deployment.yml
-oc delete -f ocp/frontend-v2-deployment.yml
-oc delete -f ocp/frontend-service.yml
-oc delete -f ocp/frontend-route.yml
+oc delete -f ocp/backend-v1-deployment.yml -n $USERID
+oc delete -f ocp/backend-v2-deployment.yml -n $USERID
+oc delete -f ocp/backend-service.yml -n $USERID
+oc delete -f ocp/frontend-v1-deployment.yml -n $USERID
+oc delete -f ocp/frontend-v2-deployment.yml -n $USERID
+oc delete -f ocp/frontend-service.yml -n $USERID
+oc delete -f ocp/frontend-route.yml -n $USERID
